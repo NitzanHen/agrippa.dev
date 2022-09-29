d51bb6030b68effac4341c4fa589ead7a1e3c12f
Nitzan Hen <code@nitzanhen.net>
2022-09-29 13:58:56 +0300

    Dep updates & setup mdx

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,21 @@
 import { defineConfig } from 'astro/config';
 import preact from '@astrojs/preact';
 import react from '@astrojs/react';
-
 import solid from "@astrojs/solid-js";
+
+import mdx from "@astrojs/mdx";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [// Enable Preact to support Preact JSX components.
-  preact(), // Enable React for the Algolia search component.
-  react(), solid()]
+  integrations: [
+    preact(),
+    react(),
+    solid(),
+    mdx()
+  ],
+  site: process.env.SITE_URL ?? 'http://localhost:3000', 
+  legacy: {
+    /** @todo remove this */
+    astroFlavoredMarkdown: true,
+  },
 });
